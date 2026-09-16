de8bf713d148c43f7543ed712814af073910ab30
Nejc <nejc.susnik05@gmail.com>
2026-09-16 21:25:01 +0200

    fix

--- src/main.c
+++ src/main.c
@@ -1,56 +1,63 @@
 #include "syscalls/filesystem.h"
-#include "memory.h"
+#include <stdio.h>
 #include <sys/abi/filesystem.h>
 #include "syscalls/syscall_generic.h"
 
-int strlen(const char *str) {
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
-}
-
 int main() {
-
-    char *test_line = "Hello, World!\n";
-
-    char buffer[1024];
+    char buffer[10];
+    // uint64_t tty_path_len = strlen(tty_path);
+    // int tty_fd = _fopen(tty_path_len, tty_path, 0, make_open_flags(1, 1, 0, 0));
+    // if (tty_fd < 0) {
+    //     // Handle error
+    //     return -1;
+    // }
+    //
+    // while (1) {
+    //     uint64_t bytes_written = _fwrite(tty_fd, strlen(test_line), test_line);
+    //
+    //     if (bytes_written >= ((uint64_t)1 << 63)) {
+    //         // Handle error
+    //         break;
+    //     }
+    //
+    //     syscall_2ret ret = _fread(tty_fd, 1024, buffer, make_read_flags(0));
+    //     uint64_t bytes_read = ret.ret0;
+    //     uint64_t ret_result = ret.ret1;
+    //     if (bytes_read >= ((uint64_t)1 << 63)) {
+    //         // Handle error
+    //         break;
+    //     }
+    //
+    //     if (bytes_read > 0) {
+    //         _fwrite(tty_fd, bytes_read, buffer);
+    //     }
+    //
+    //     if (ret_result == READ_RESULT_PERMANENT_EOF) {
+    //         break;
+    //     }
+    // }
+    //
+    // _fclose(tty_fd);
     
-    char *tty_path = "/tty";
-    uint64_t tty_path_len = strlen(tty_path);
-    int tty_fd = _fopen(tty_path_len, tty_path, 0, make_open_flags(1, 1, 0, 0));
-    if (tty_fd < 0) {
+
+    FILE *tty_read = fopen("/tty", "r");
+    FILE *tty_write = fopen("/tty", "a");
+
+    if (tty_read == NULL || tty_write == NULL) {
         // Handle error
         return -1;
     }
 
     while (1) {
-        uint64_t bytes_written = _fwrite(tty_fd, strlen(test_line), test_line);
-
-        if (bytes_written >= ((uint64_t)1 << 63)) {
-            // Handle error
-            break;
-        }
-
-        syscall_2ret ret = _fread(tty_fd, 1024, buffer, make_read_flags(0));
-        uint64_t bytes_read = ret.ret0;
-        uint64_t ret_result = ret.ret1;
-        if (bytes_read >= ((uint64_t)1 << 63)) {
-            // Handle error
-            break;
-        }
-
+        size_t bytes_read = fread(buffer, 1, sizeof(buffer), tty_read);
         if (bytes_read > 0) {
-            _fwrite(tty_fd, bytes_read, buffer);
+            fwrite(buffer, 1, bytes_read, tty_write);
         }
 
-        if (ret_result == PERMANENT_EOF) {
+        if (feof(tty_read)) {
             break;
         }
     }
-
-    _fclose(tty_fd);
 
     return 0;
 }
